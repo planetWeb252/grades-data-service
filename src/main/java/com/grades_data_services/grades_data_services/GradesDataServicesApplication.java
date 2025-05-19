db5b8606cae9bac5c2778902f6a989697dadbfa5
planetWeb252 <juanjosefrancojimenez@outlook.com>
2025-05-19 15:38:08 +0200

    repo with dependencies

---
+++ src/main/java/com/grades_data_services/grades_data_services/GradesDataServicesApplication.java
@@ -0,0 +1,13 @@
+package com.grades_data_services.grades_data_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradesDataServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradesDataServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grades_data_services/grades_data_services/GradesDataServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grades_data_services.grades_data_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradesDataServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
